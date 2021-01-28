8f27a510194184d4aefa973b053f58e1fd6ea0ac
Knud Rishøj <knud@det-gamle-posthus.dk>
2021-01-28 12:14:56 +0100

    Initial Commit

---
+++ includes/class-tobenski-event-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/tobenski/
+ * @since      1.0.0
+ *
+ * @package    Tobenski_Event
+ * @subpackage Tobenski_Event/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Tobenski_Event
+ * @subpackage Tobenski_Event/includes
+ * @author     Knud Rishøj <tirdyr@tobenski.dk>
+ */
+class Tobenski_Event_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
